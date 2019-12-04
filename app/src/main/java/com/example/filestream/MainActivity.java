205692a9753d288de7ae69055255feb11fdc94ad
lzntc130 <592322736@qq.com>
2019-12-04 10:37:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/filestream/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.filestream;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button=(Button) findViewById(R.id.write);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OutputStream out=null;
+                try {
+                    FileOutputStream fileOutputStream=openFileOutput("a.txt",MODE_PRIVATE);
+                    out=new BufferedOutputStream(fileOutputStream);
+                    String content="lzn 2017010316";
+                    try {
+                        out.write(content.getBytes(StandardCharsets.UTF_8));
+                    }
+                    finally {
+                        if(out!=null)
+                            out.close();
+                    }                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }        });
+        Button button2=(Button) findViewById(R.id.read);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                InputStream in=null;
+                try {
+                    FileInputStream fileInputStream = openFileInput("a.txt");
+                    in=new BufferedInputStream(fileInputStream);
+                    int c;
+                    StringBuilder stringBuilder=new StringBuilder("");
+                    try{
+                        while ((c=in.read())!=-1) {
+                            stringBuilder.append((char)c);
+                        }
+                        Toast.makeText(MainActivity.this,stringBuilder.toString(),Toast.LENGTH_LONG).show();
+                    }
+                    finally {
+                        if(in!=null)
+                            in.close();
+                    }
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+}
